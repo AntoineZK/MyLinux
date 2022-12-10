0004217cb51c60fc280f8bb6377f682f7589d220
AntoineZK_WSL <antoine.khalidy@epitech.eu>
2022-12-10 03:32:21 +0100

    [ADD] htop + python + -h + oh-my-zsh

--- install.sh
+++ install.sh
@@ -1,11 +1,22 @@
-#!/bin/bash          
-RED='\033[0;31m'
+#!/bin/bash
+Red='\033[0;31m'
 NC='\033[0m'
 Green='\033[0;32m'
 Blue='\033[0;34m'
-
 Distrib='Unknown'
 
+Help="./install.sh [apt or dnf] [omz] [--help or -h] [-omz]"
+
+if [ "$1" = "-omz" ]
+then
+    printf "${Blue}Will install oh-my-zsh on your computer${NC}\n"
+    exit 0
+fi
+if [ "$1" = "--help" ] | [ "$1" = "-h" ]
+then
+    printf "${Blue}${Help}${NC}\n"
+    exit 0
+fi
 if [ "$1" = "apt" ]
 then
     echo "Distribution set to Ubuntu"
@@ -15,14 +26,14 @@ then
     echo "Distribution set to Fedora"
     Distrib='dnf'
 else
-    echo "Unknown distribution"
+    printf "${Red}${Help}${NC}\n"
     exit 84
 fi
 
 $Distrib update
 if [ $? -eq 100 ]
 then
-    echo "Permission denied. Please use sudo or root user."
+    printf "${Red}Permission denied. Please use sudo or root user.${NC}\n"
     exit 1
 fi
 $Distrib upgrade -y
@@ -35,7 +46,16 @@ $Distrib install zsh -y
 $Distrib install curl wget -y
 $Distrib install gcc -y
 $Distrib install emacs -y
+$Distrib install htop -y
+$Distrib install python3 -y
 $Distrib install libncurses5-dev libncursesw5-dev -y
 $Distrib autoremove -y
 echo "alias emacs="emacs -nw"" >> ~/.zshrc
 printf "${Green}Installation complete${NC}\n"
+
+if [ "$2" = "omz" ]
+then
+    printf "${Green}Start installation of oh-my-zsh${NC}\n"
+    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    printf "${Green}Installation complete${NC}\n"
+fi
