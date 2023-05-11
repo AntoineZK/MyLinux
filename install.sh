4453bce364ebf595f9f5c91ef4d6ef46ba0e615d
AntoineZK <antoine.khalidy@epitech.eu>
2023-05-11 16:09:19 +0200

    [FIX] bug root zsh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ Green='\033[0;32m'
 Blue='\033[0;34m'
 Distrib='Unknown'
 
-Help="./install.sh [apt or dnf] [--help or -h]"
+Help="./install.sh [apt or dnf] [--help or -h] [--omzGet or --omzCurl]"
 
 if [ "$1" = "--help" ] | [ "$1" = "-h" ]
 then
@@ -25,44 +25,47 @@ else
     exit 84
 fi
 
-$Distrib update
-if [ $? -eq 100 ]
-then
-    printf "${Red}Permission denied. Please use sudo or root user.${NC}\n"
-    exit 1
-fi
-$Distrib upgrade -y
-$Distrib install apt-utils -y
-$Distrib install make cmake -y
-$Distrib install valgrind -y
-$Distrib install libcsfml-dev libsfml-dev -y
-$Distrib install git -y
-$Distrib install zsh -y
-$Distrib install curl wget -y
-$Distrib install gcc g++ -y
-$Distrib install emacs -y
-$Distrib install htop -y
-$Distrib install python3 -y
-$Distrib install libncurses5-dev libncursesw5-dev -y
-$Distrib install net-tools -y
-$Distrib install python3-pip -y
-$Distrib install nano -y
-$Distrib install nasm -y
-$Distrib install neofetch -y
-$Distrib install tcsh -y
-$Distrib install tree -y
-$Distrib autoremove -y
+sudo $Distrib update
+sudo $Distrib upgrade -y
+sudo $Distrib install apt-utils -y
+sudo $Distrib install make cmake -y
+sudo $Distrib install valgrind -y
+sudo $Distrib install libcsfml-dev libsfml-dev -y
+sudo $Distrib install git -y
+sudo $Distrib install zsh -y
+sudo $Distrib install curl wget -y
+sudo $Distrib install gcc g++ -y
+sudo $Distrib install emacs -y
+sudo $Distrib install htop -y
+sudo $Distrib install python3 -y
+sudo $Distrib install libncurses5-dev libncursesw5-dev -y
+sudo $Distrib install net-tools -y
+sudo $Distrib install python3-pip -y
+sudo $Distrib install nano -y
+sudo $Distrib install nasm -y
+sudo $Distrib install neofetch -y
+sudo $Distrib install tcsh -y
+sudo $Distrib install tree -y
+sudo $Distrib autoremove -y
 
 echo "alias emacs="emacs -nw"" >> ~/.zshrc
 printf "${Green}Installation complete${NC}\n"
 
-printf "${Green}Start installation of oh-my-zsh${NC}\n"
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-if [ $? -eq 100 ]
+if [ -d ~/.oh-my-zsh ]
 then
+    printf "${Blue}oh-my-zsh already installed${NC}\n"
+    exit 0
+elif [ $2 = "--omzGet" ]
+then
+    printf "${Green}Start installation of oh-my-zsh${NC}\n"
     sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+    printf "${Green}Installation complete${NC}\n"
+    exit 0
+elif [ $2 = "--omzCurl" ]
+then
+    printf "${Green}Start installation of oh-my-zsh${NC}\n"
+    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    printf "${Green}Installation complete${NC}\n"
+    exit 0
 fi
-printf "${Green}Installation complete${NC}\n"
-
-#curl / wget global
 # install coding style
